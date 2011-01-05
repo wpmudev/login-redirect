ff17cfd34ee9651a30609009878521bdd7ce63f7
ulrich <ulrich@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-01-05 07:18:04 +0000

    Login Redirect 1.0.2
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/login-redirect@1928 f53fe46e-8c02-459b-9fdd-93e75771b278

--- login-redirect.php
+++ login-redirect.php
@@ -1,14 +1,16 @@
 <?php
 /*
 Plugin Name: Login Redirect
-Plugin URI: 
-Description:
-Author: Andrew Billits
-Version: 1.0.1
-Author URI:
+Plugin URI: http://premium.wpmudev.org/project/login-redirect
+Description: Redirects users to specified url after they've logged in, replacing the default 'go to dashboard' behavior.
+Author: Andrew Billits, ulrich sossou
+Version: 1.0.2
+Text Domain: login_redirect
+Author URI: http://premium.wpmudev.org/
+WDP ID: 43
 */
 
-/* 
+/*
 Copyright 2007-2009 Incsub (http://incsub.com)
 
 This program is free software; you can redistribute it and/or modify
@@ -25,39 +27,89 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-//------------------------------------------------------------------------//
-//---Config---------------------------------------------------------------//
-//------------------------------------------------------------------------//
+/**
+ * Plugin main class
+ **/
+class Login_Redirect {
 
-$login_redirect_url = 'http://domain.tld'; // the url you want users to be redirected too after logging in
-
-//------------------------------------------------------------------------//
-//---Hook-----------------------------------------------------------------//
-//------------------------------------------------------------------------//
-add_action('login_head', 'login_redirect_to', 1);
-add_filter('wp_redirect', 'login_redirect');
-//------------------------------------------------------------------------//
-//---Functions------------------------------------------------------------//
-//------------------------------------------------------------------------//
-
-function login_redirect($location){
-	global $login_redirect_url;
-	if ( $location == 'login-redirect' ) {
-		$location = $login_redirect_url;
+	/**
+	 * PHP 4 constructor
+	 **/
+	function Login_Redirect() {
 	}
-	return $location;
-}
 
-function login_redirect_to(){
-	if ( empty( $_GET['redirect_to'] ) && empty( $_GET['action'] ) && empty( $_GET['username'] ) && empty( $_GET['checkemail'] ) ) {
-		if ( empty( $_POST['pwd'] ) && empty( $_POST['log'] ) ) {
-			echo "
-			<SCRIPT LANGUAGE='JavaScript'>
-			window.location='wp-login.php?redirect_to=login-redirect';
-			</script>
-			";
+	/**
+	 * PHP 5 constructor
+	 **/
+	function __construct() {
+		add_filter( 'login_redirect', array( &$this, 'redirect' ), 10, 3 );
+		add_action( 'wpmu_options', array( &$this, 'network_option' ) );
+		add_action( 'update_wpmu_options', array( &$this, 'update_network_option' ) );
+
+		// load text domain
+		if ( defined( 'WPMU_PLUGIN_DIR' ) && file_exists( WPMU_PLUGIN_DIR . '/login-redirect.php' ) ) {
+			load_muplugin_textdomain( 'login_redirect', 'login-redirect-files/languages' );
+		} else {
+			load_plugin_textdomain( 'login_redirect', false, dirname( plugin_basename( __FILE__ ) ) . '/login-redirect-files/languages' );
 		}
 	}
+
+	/**
+	 * Redirect user on login
+	 **/
+	function redirect( $redirect_to, $requested_redirect_to, $user ) {
+		$interim_login = isset( $_REQUEST['interim-login'] );
+		$reauth = empty( $_REQUEST['reauth'] ) ? false : true;
+		$login_redirect_url = get_site_option( 'login_redirect_url' );
+
+		if ( !is_wp_error( $user ) && !$reauth && !$interim_login && !empty( $login_redirect_url ) ) {
+			wp_redirect( $login_redirect_url );
+			exit();
+		}
+
+		return $redirect_to;
+	}
+
+	/**
+	 * Network option
+	 **/
+	function network_option() {
+		?>
+		<h3><?php _e( 'Login Redirect', 'login_redirect' ); ?></h3>
+		<table class="form-table">
+			<tr valign="top">
+				<th scope="row"><label for="login_redirect_url"><?php _e( 'Redirect to', 'login_redirect' ) ?></label></th>
+				<td>
+					<input name="login_redirect_url" type="text" id="login_redirect_url" value="<?php echo esc_attr( get_site_option( 'login_redirect_url' ) ) ?>" size="40" />
+					<br />
+					<?php _e( 'The URL users will be redirected to after login.', 'login_redirect' ) ?>
+				</td>
+			</tr>
+		</table>
+		<?php
+	}
+
+	/**
+	 * Save option in the option
+	 **/
+	function update_network_option() {
+		update_site_option( 'login_redirect_url', stripslashes( $_POST['login_redirect_url'] ) );
+	}
+
 }
 
-?>
+$login_redirect =& new Login_Redirect();
+
+/**
+ * Show notification if WPMUDEV Update Notifications plugin is not installed
+ *
+ **/
+if ( !function_exists( 'wdp_un_check' ) ) {
+	add_action( 'admin_notices', 'wdp_un_check', 5 );
+	add_action( 'network_admin_notices', 'wdp_un_check', 5 );
+
+	function wdp_un_check() {
+		if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'edit_users' ) )
+			echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
+	}
+}
